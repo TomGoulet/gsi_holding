bad2f05ccf23ffd64faa33037f4a5d368cdc8082
Thomas GOULET <t.gouletpro@gmail.com>
2019-05-20 18:25:27 +0200

    Update v1

---
+++ application/views/accueil.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Welcome to CodeIgniter</title>
+  <link rel="stylesheet" href="assets/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/dist/js/bootstrap.min.js">
+  <link rel="stylesheet" type="text/css" href="assets/dist/css/accueil.css">
+<head>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<div class="container-fluid">
+  <div class="row">
+    <div class="text-description col-xs-12 col-lg-6">
+      <h1>Group Sofra Invest</h1>
+      <h2>Synergie des métiers pour la gestion du poste client</h2>
+      <p>Group Sofra Invest, via ses filiales, SOFRA, SNI et TDA SERVICES, vous propose des prestations pour la gestion de votre clientèle que ce soit des relances commerciales en marque blanche, pour la gestion de vos impayés et du recouvrement, pour des recherches et investigations civiles et commerciales, pour la prise en charge de vos plis non distribués, le traitement de vos PND ou l’enrichissement de votre base client.</p>
+      <a href="/pages/concept">
+        <button class="float-right align-middle rounded-pill" name="button" type="button" class="button-style">En savoir plus</button>
+      </a>
+    </div>
+    <div class ="image description col-xs-12 col-lg-6">
+    <img class ="gsi_accueil_image" src="assets/dist/images/18784.jpg" alt="">
+    </div>
+  </div>
+</div>
+
+
+
